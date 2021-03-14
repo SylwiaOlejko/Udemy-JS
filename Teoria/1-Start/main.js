e4971f27bcca0d7278496fbb0775ff73d825963e
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-03-14 01:12:03 +0100

    najmniejsza skłądowa programu

---
+++ Teoria/1-Start/main.js
@@ -0,0 +1,11 @@
+/*Dodawanie do pliku HTML*/
+
+//<script src="path/to/file.js"></script>
+
+console.log("ok");
+
+/* INSTRUKCJA - najmniejsza składowa programu */
+
+console.log (2 + 2);
+console.log ("2" + "2");
+let result = 2 + 2;
