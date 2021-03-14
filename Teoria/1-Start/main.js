1987c18321356e21d2acb9e6420511c609536926
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-03-14 01:34:28 +0100

    notacja wielbładzia

--- Teoria/1-Start/main.js
+++ Teoria/1-Start/main.js
@@ -7,5 +7,39 @@ console.log("ok");
 /* INSTRUKCJA - najmniejsza składowa programu */
 
 console.log (2 + 2);
-console.log ("2" + "2");
+//console.log ("2" + "2");
 let result = 2 + 2;
+
+"teskt".toUpperCase();
+
+/*
+-instrukcja może coś zwracać ale nie musi. Widac to dobrze w konsoli.
+-zadanie, pojedyńczy element programu. Instrukcja ma sens o ile wpływa na program.
+-średnik kończy instrukcję, jeśli nie napiszemy to zostanie dodany w prosenie wykonywania kodu zostaną dodane.
+
+//const users = []
+//let points = 10 // źle
+
+//const users = [];
+//let points = 10; //dobrze
+
+-instrukcje sa wykonywane po kolei z góry na dół tak jest czytany program
+*/
+
+/* Notacja wielbłądzia (CAMEL CASE)*/
+"cos".toUpperCase();
+const variableName = "tutaj zawartość";
+
+/* KOMENTARZE */
+
+/*
+-ignorowanie przez interpreter
+-pisz, bo za miesiac nie będziesz pamietał co robiłes i dlaczego :)
+-samokomentujący się kod? Nazwy, których używasz mają byc zrozumiałe
+const userID = 1023l;
+function upadereScore(){}
+let lastLogin = "11.09.2018"
+
+const a1 = "Tomasz"
+function add(){}
+function addUsersToCourse(){}
