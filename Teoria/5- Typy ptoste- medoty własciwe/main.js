cae1eb5122ec90c5c4113b6bf5ff4166b2aa6dad
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-03-24 22:54:14 +0100

    literał łańcucha

---
+++ Teoria/5- Typy ptoste- medoty własciwe/main.js
@@ -0,0 +1,9 @@
+const txt = 'jakiś tekst nowy1';
+let score = 12.55;
+//te dwie zmienne przydadzą nam się w dalszych przykładach w pliku
+
+"zbiór znaków w cudzysłowie"; 1222; true; null; undefined;
+// Każdy typ prosty (poza Symbol) można stworzyć za pomocą literału. Literał to zestaw znaków, które zostaną rozpoznane przez interpreter
+
+//literał łancucha, literał liczby ... 
+// na przyszłość: literał obiektu "{}", literał tablicy "[]"
