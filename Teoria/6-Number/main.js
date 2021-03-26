56b2cb7497528504e2cc17206b99ed09f873c610
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-03-26 16:30:06 +0100

    literał

---
+++ Teoria/6-Number/main.js
@@ -0,0 +1,10 @@
+//  za pomocą literału, po prostu liczba utworzy wartość typu prostego
+
+const integerExample = 100;
+const decimalExample = 100.212;
+typeof integerExample; //number w JS, w innych językach często integer dla liczb całkowitych 
+typeof decimalExample; //number w JS, w innych językach często float czy decimal
+
+// JS akceptuje różne systemy liczbowe (poza dziesętnym)
+// Reprezentacja szesznastkowa (prefiks 0x)
+const hexNumber = 0xff; //znamy z css czyli ff to 255 w systemie dziesiętnym
