141a711447b2fff5bb91a97d085b5e68d2e18e0b
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-03-31 12:42:20 +0200

    string

---
+++ Teoria/6,5-Typ-String/main.js
@@ -0,0 +1,11 @@
+// Literał tekstowy - sposób tworzenia wartości tpyu prostego
+
+
+// STRING - kolekcja znaków znajdujących się wewnątrz cudzysłowów (pojedyńczych (apostrof) (''), podwójnych ("") i grawisa (``)) Ps. grawis jest w tym samym kalwiszu co tylda (~) czyli zazwyczaj pod "ESC";
+
+
+// String na polski: łańcuch zanków, ciąg znaków, zbiór znaków, lub po prostu "lańcuch"
+
+const userName = "Tomasz Niewierny";
+const userName2 = 'Miszko Pierwszy'
+const userName3 = `Jaś Fasola` // super string od ES6 - innym razem bedzie na ten temat  materiał 
