44eb94901636adfba97a8cc431fa02044821ac46
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-09 17:06:59 +0200

    if

--- Teoria/9-Instrukcja warunkowa/main.js
+++ Teoria/9-Instrukcja warunkowa/main.js
@@ -1,27 +1,26 @@
 /*Bardzo często potrzebujemy uruchomić różny kod(różny blok kodu) w zależności od stanu (sytuacji,  spełnienia, warunku) w programie */
 
-//Instrukcja warunkowa IF
+// INSTRUKCJA WARUNKOWA IF
 
-//Przykład
-const condition =2;
+// Przykład1
+const condition = 2;
+
 if (condition) {
-    
-    console.log("widzisz mnie, poniewaz warunek jest prawdziwy i wykonuje zawartość bloku instrukcji warunkowej");
-    alert("działa!");
+ // alert("działa!");
+ console.log("widzisz mnie ponieważ warunek jest prawdziwy i wykonuje zawartość bloku instrukcji warunkowej");
 }
 
-//Kod w bloku jest uruchomiony TYLKO WTEDY gdy warunek jest prawdziwy. Jeśli nie jest prawdziwy to blok jest pomijany ({} - pomijany)
 
-//Przykład 2
+// Kod w bloku jest uruchomiony TYLKO WTEDY gdy warunek jest prawdziwy. Jeśli nie jest prawdziwy, to blok jest pomijany ({} - pomijany)
+
+// Przykład 2
 const userName = "Dobromir";
 
-if (userName){
-    console.log("Witaj" + userName + "!");
+if (userName) {
+ console.log("Witaj " + userName + "!")
 }
+// to samo, co wyżej z innaczej skonstruowanym warunkiem jednak oznaczajacym to samo.
 
-//to samo, co  wyzej z inaczej skonstuowanym warunkiem jednak oznaczającym to samo
-
-if (userName !== ""){
-    console.log("Witaj" + userName)
+if (userName !== "") {
+ console.log("Witaj " + userName)
 }
-
