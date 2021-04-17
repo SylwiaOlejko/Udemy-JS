e63688e3bddfef9df21b091ae5cec3a2543e3f15
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-17 17:23:12 +0200

    co to jestt tablica

---
+++ Teoria/10-Tablice wprowadzenie/main.js
@@ -0,0 +1,7 @@
+// TABLICA - ARRAY
+
+// Co trzeba wiedzieć o tablicy
+// -- jest to obiekt wbudowany (ma więc właściwości i metody) 
+// -- posiada uporządkową (kolejność jest ważna) zbiór wartości
+// -- każda przechowywana wartość w tablicy ma swój indeks. Pierwsza wartość ma indeks 0
+
