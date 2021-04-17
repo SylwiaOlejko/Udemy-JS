45c2230f93383e7641b5587685f2960720555d3c
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-17 17:25:46 +0200

    tworzenie tablicy

--- Teoria/10-Tablice wprowadzenie/main.js
+++ Teoria/10-Tablice wprowadzenie/main.js
@@ -5,3 +5,37 @@
 // -- posiada uporządkową (kolejność jest ważna) zbiór wartości
 // -- każda przechowywana wartość w tablicy ma swój indeks. Pierwsza wartość ma indeks 0
 
+// TWORZENIE TABLICY
+
+// Literał tablicy z podaniem zawartości
+
+const nameList = ["Ludomir", "Żyrosław", "Gniewomir", "Myślimir"];
+
+// Literał pustej tablicy a potem uzupełnienie za pomocą odwołania się do indeksu
+const cityList = [];
+cityList[0] = 'Konin';
+cityList[1] = 'Zabrze';
+
+// Konstruktor - pusta tablica
+const items = [];
+const gameItems = new Array();
+// Konstruktor z uzupełnianonymi elementami
+const colors = new Array("red", "blue");
+colors[2] = "green";
+
+
+// W każdym z tych sposobów powstaje instacja Tablicy o tych samych właściwościach i metodach (co widzimy po konstruktorze).
+// console.log(typeof nameList);
+// console.log(nameList);
+// console.log(typeof colors);
+// console.log(colors);
+
+
+// Pamietajmy, że możemy edytować tablicę nawet jeśli jest ona przypisana do zmiennej const. Najczęściej więc wybierzemy dla tablicy zmienną const. Nie możemy jednak nic innego przypisać do tej zmiennej
+const myArrayConst = [];
+let myArrayLet = [];
+
+myArrayConst[0] = "pierwszy const";
+myArrayLet[0] = "pierwszy let";
+// myArrayConst = "coś innego";
+// myArrayLet = "coś innego";
