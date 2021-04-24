ea5c9e38f2e90e8f52b733179a4c9bca9d1f62f9
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-24 14:55:40 +0200

    Unshift- dodanie elementu na początku tablicy

--- Teoria/11-Tablica metody z komentarzami/main.js
+++ Teoria/11-Tablica metody z komentarzami/main.js
@@ -1,9 +1,15 @@
 // TABLICA - METODY
 
 const italianUsers = ["Giovanni", "Michele", "Angelo", "Luigi", "Valentina", "Marco"];
-console.log(italianUsers);
+//console.log(italianUsers);
 
 // PUSH - dodaj element na końcu tablicy
 
-// italianUsers.push("Alessandra");
+ //italianUsers.push("Alessandra");
+ //console.log(italianUsers);
+
+
+ // UNSHIFT - dodaj element na początku tablicy
+
+// italianUsers.unshift("Tommaso");
 // console.log(italianUsers);
