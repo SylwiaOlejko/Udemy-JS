e562d272ad4ab7624281f09e8bd3b4a7c83786d8
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-24 14:49:57 +0200

    Push

--- Teoria/11-Tablica metody z komentarzami/main.js
+++ Teoria/11-Tablica metody z komentarzami/main.js
@@ -1,4 +1,9 @@
 // TABLICA - METODY
 
 const italianUsers = ["Giovanni", "Michele", "Angelo", "Luigi", "Valentina", "Marco"];
+console.log(italianUsers);
+
+// PUSH - dodaj element na końcu tablicy
+
+// italianUsers.push("Alessandra");
 // console.log(italianUsers);
