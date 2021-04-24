eb7b6e2d6b0e018660578dde171d322a919f4b12
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-24 14:59:08 +0200

    POP - usuń ostatni element z tablicy

--- Teoria/11-Tablica metody z komentarzami/main.js
+++ Teoria/11-Tablica metody z komentarzami/main.js
@@ -11,5 +11,15 @@ const italianUsers = ["Giovanni", "Michele", "Angelo", "Luigi", "Valentina", "Ma
 
  // UNSHIFT - dodaj element na początku tablicy
 
-// italianUsers.unshift("Tommaso");
+ //italianUsers.unshift("Tommaso");
+ //console.log(italianUsers);
+
+ // POP - Usuń ostatni element tablicy
+// Usunięcie wartości i indeksu (nie tylko wartości)
+
+// const deletedUser = italianUsers.pop()
 // console.log(italianUsers);
+
+// delete italianUsers[italianUsers.length - 1] //zmiana wartości ostatniego  na udenfined
+
+/* ---------------------------------  */
