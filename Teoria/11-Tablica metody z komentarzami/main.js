ecba33c32efa308bcfbf1749501d6d3496ef25e3
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-04-24 15:42:04 +0200

    shift - usuń pierwszy element

--- Teoria/11-Tablica metody z komentarzami/main.js
+++ Teoria/11-Tablica metody z komentarzami/main.js
@@ -17,9 +17,17 @@ const italianUsers = ["Giovanni", "Michele", "Angelo", "Luigi", "Valentina", "Ma
  // POP - Usuń ostatni element tablicy
 // Usunięcie wartości i indeksu (nie tylko wartości)
 
-// const deletedUser = italianUsers.pop()
-// console.log(italianUsers);
+//const deletedUser = italianUsers.pop()
+//console.log(italianUsers);
 
-// delete italianUsers[italianUsers.length - 1] //zmiana wartości ostatniego  na udenfined
+//delete italianUsers[italianUsers.length - 1] //zmiana wartości ostatniego  na udenfined
 
 /* ---------------------------------  */
+
+// SHIFT - Usuń pierwszy element
+// Usunięcie wartości i indeksu (nie tylko wartości)
+
+// italianUsers.shift();
+// italianUsers.shift();
+// console.log(italianUsers);
+// delete italianUsers[0] //zmiana wartości pierwszego na udenfined
