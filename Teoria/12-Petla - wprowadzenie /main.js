cff0f4a97a7c2fc938d0947d95c6d5d9cb22370f
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-17 23:35:42 +0200

    powiekszanie pętli

--- Teoria/12-Petla - wprowadzenie /main.js
+++ Teoria/12-Petla - wprowadzenie /main.js
@@ -12,7 +12,14 @@
 } */
 // for - słowo kluczowe
 
-for (let i = 0; i <= 2; i++) {
+for (let i = 0; i < 10; i++) {
     console.log("wyświetlenie " + i);
    }
    
+
+   // Działanie
+// 1 - inicjalizacja następuje raz (konwencja mówi, że nazywamy iterator zmienną i. Powinien to być let, a nie var czy bez deklaracji). Najczęściej deklarujemy iterator jako 0.
+// 2 - warunek jest sprawdzany za każdym razem (przed wykoneniem bloku). Warunek jest konwertowany do true/flase. Jeśli false pętla kńczy działanie a program idzie dalej. Jeśli true, program idzie do bloku kodu
+// 3 - Wykonanie kodu ( o ile warunek jest prawdziwy)
+// 4 - po wykonaniu bloku zwiększa iterator (najczęsciej o 1 za pomocą inkrementacji, ale nie jest to konieczne)
+// DALEJ: Nowa pętla od punktu 2 (inicjalizacja jest robiona tylko raz)
