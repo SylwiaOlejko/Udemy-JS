27d6f4ed3982c6d5f63a407b7ec717ff4d7d9b29
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-17 23:15:49 +0200

    loop - pętla

---
+++ Teoria/12-Petla - wprowadzenie /main.js
@@ -0,0 +1,6 @@
+//  LOOPS - PĘTLE
+
+// Pętla umożliwia wielokrotne wykonanie kodu.
+// Pętla pozwala wykonywać blok kodu wielokrotnie, tyle razy ile spełniony jest określony warunek.
+// Pętla pozwala pracować na elementach tablicy czy obiektu.
+// Pętla opiera się na sprawdzeniu warunku. Pętla jest tak długo wykonywana jak spełniony jest warunek
