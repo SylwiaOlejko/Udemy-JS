02c1e786cc40988d77c1c881ee4c1812909b72f0
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-17 23:22:16 +0200

    for

--- Teoria/12-Petla - wprowadzenie /main.js
+++ Teoria/12-Petla - wprowadzenie /main.js
@@ -4,3 +4,15 @@
 // Pętla pozwala wykonywać blok kodu wielokrotnie, tyle razy ile spełniony jest określony warunek.
 // Pętla pozwala pracować na elementach tablicy czy obiektu.
 // Pętla opiera się na sprawdzeniu warunku. Pętla jest tak długo wykonywana jak spełniony jest warunek
+
+// FOR
+
+/* for (inicjalizacja iteratora; warunek; zwiększenie iteratora) {
+ blok kodu
+} */
+// for - słowo kluczowe
+
+for (let i = 0; i <= 2; i++) {
+    console.log("wyświetlenie " + i);
+   }
+   
