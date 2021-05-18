c0b74c23a1eb48c6d7ff4325e195bac8ee6e9ac7
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-18 18:02:18 +0200

    instrukcja programowa

--- Teoria/13-Petla-cz.2/main.js
+++ Teoria/13-Petla-cz.2/main.js
@@ -21,30 +21,30 @@
 
 //  Przykład2 - obliczmy wiek użytkownika
 
-const usersDateBirth = [2000, 1992, 1932, 1980, 1981, 1971, 2009];
-const currentYear = 2018;
+// const usersDateBirth = [2000, 1992, 1932, 1980, 1981, 1971, 2009];
+// const currentYear = 2021;
 
 // for (let i = 0; i < usersDateBirth.length; i++) {
-//  const userAge = currentYear - usersDateBirth[i];
-//  // console.log(userAge);
+//  const userAge = currentYear - usersDateBirth[i]; //i = iterator używamy za każdym razem żeby  dostać się do tablicy do kazdej wartości. Jeśli wpiszemy jakaś wartośc zamiast i = iteratora, to pętla bedzie nam zwracała ciągle tą wpisaną wartość
+// //   console.log(userAge);
 //  console.log(`wiek użytkownika numer ${i + 1} to ${userAge}`);
 // }
 
 //  Przykład3 - for + instrukcja warunkowe alert
 
-// const guest = ["piłkarz", "kucharz", "murarz", "programista", "polityk", "pisarz", "magazynier", "emeryt"];
+const guest = ["piłkarz", "kucharz", "murarz", "programista", "polityk", "pisarz", "magazynier", "emeryt"];
 
-// for (let i = 0; i < guest.length; i++) {
+for (let i = 0; i < guest.length; i++) {
 
-//  if (guest[i] === "programista") {
-//   console.log("przepraszam nie ma już miejsce (nie wpuszczać, (programiści to nudziarze - tylko nie mówić) ")
-//  } else if (guest[i] === "polityk") {
-//   console.log("Ojej jaka szkoda, nie ma już miejsc (po żadnym pozorem nie wpuszczać! Zepsują każdą imprezę)");
-//  } else {
-//   console.log("witamy Panie " + guest[i] + ", zapraszamy!");
-//  }
+ if (guest[i] === "programista") {
+  console.log("przepraszam nie ma już miejsce (nie wpuszczać, (programiści to nudziarze - tylko nie mówić) ")
+ } else if (guest[i] === "polityk") {
+  console.log("Ojej jaka szkoda, nie ma już miejsc (po żadnym pozorem nie wpuszczać! Zepsują każdą imprezę)");
+ } else {
+  console.log("witamy Panie " + guest[i] + ", zapraszamy!");
+ }
 
-// }
+}
 
 
 // PĘTLA FOR - OF (ES6)
