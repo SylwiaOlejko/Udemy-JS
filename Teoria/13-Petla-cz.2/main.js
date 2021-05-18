047341bf69b5dff8922d7f0543846c1af16f9bb8
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-18 17:47:47 +0200

    pobieranie z tablicy wieku użytkownika

--- Teoria/13-Petla-cz.2/main.js
+++ Teoria/13-Petla-cz.2/main.js
@@ -1,23 +1,23 @@
 // Pętla w pętli
 
- for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 10; j++) {
-   console.log(`zmienna i czyli ${i} pomnożona przez zmienną j czyli ${j} da wynik ${i * j}`);
-  }
- }
+//  for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 10; j++) {
+//    console.log(`zmienna i czyli ${i} pomnożona przez zmienną j czyli ${j} da wynik ${i * j}`);
+//   }
+//  }
 
  
 // Pętla na tablicy
 
 // Przykład1 wyświetlamy wiek użytkownika
 
-// const usersAge = [19, 18, 15, 45, 34, 33, 25];
+const usersAge = [19, 18, 15, 45, 34, 33, 25];
 
-// for (let i = 0; i < usersAge.length; i++) {
-//  // console.log(`użytkownik o indeksie ${i} jest w wieku ${usersAge[i]}`);
+for (let i = 0; i < usersAge.length; i++) {
+ console.log(`użytkownik o indeksie ${i} jest w wieku ${usersAge[i]}`); // tablica [6]
 
-//  console.log('użytkownik o indeksie ' + i + ' jest w wieku ' + usersAge[i]);
-// }
+ //console.log('użytkownik o indeksie ' + i + ' jest w wieku ' + usersAge[i]);
+}
 
 //  Przykład2 - obliczmy wiek użytkownika
 
@@ -49,8 +49,8 @@
 
 // PĘTLA FOR - OF (ES6)
 
-const colors = ["red", "green", "black", "yellow", "white", "gold", "silver", "brown"]
+// const colors = ["red", "green", "black", "yellow", "white", "gold", "silver", "brown"]
 
-for (const color of colors) {
- console.log("mój ulubiony kolor to: " + i);
-}
+// for (const color of colors) {
+//  console.log("mój ulubiony kolor to: " + i);
+// }
