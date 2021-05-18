0a5f58ec43a8536ef51455d6dc3f8098e899e41c
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-18 17:30:19 +0200

    pętla w pętli

---
+++ Teoria/13-Petla-cz.2/main.js
@@ -0,0 +1,7 @@
+// Pętla w pętli
+
+// for (let i = 0; i < 3; i++) {
+//  for (let j = 0; j < 10; j++) {
+//   console.log(`zmienna i czyli ${i} pomnożona przez zmienną j czyli ${j} da wynik ${i * j}`);
+//  }
+// }
