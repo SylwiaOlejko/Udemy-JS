6dfdd7e4464f7ae6f77da838053cb1a8d97f3910
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-20 16:07:25 +0200

    funkcje

---
+++ Teoria/14-Funkcje,tworzenie, wywoływanie, parametry z komendami/main.js
@@ -0,0 +1,14 @@
+/* FUNKCJE */
+
+// Co o funkcjach trzeba wiedzieć na początek:
+
+// -- są obiektami (jak tablica, któą już poznaliśmy)
+
+// -- można je wywołać (jedyny obiekt, który można wywołać) -- nazwaFunkcji()
+
+// -- o funkcjach należy myśleć jako o małych programach które coś robią z danymi lub coś zwracają (lub robią i to i to). Funkcja może modyfikować program (nazywamy to efektem ubocznym) lub go nie modifikować a jedynie przetwarzać elementy zwracajać nową wartość (służy do tego słowo kluczowe return).
+
+// -- funkcje są wielokrotnego użytku (funkcja może być wywołana/uruchomiona wielokrotnie) 
+
+// -- można ich używać tam gdzie innych obiektów (są first-class objects, czyli są pełnoprawnymi obiektami) np. przypisać do zmiennych, jako argumentu w innej funkcji czy jako wartość zwracana z funkcji.
+
