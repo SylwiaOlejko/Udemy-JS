6fa4015e1fef4805a6c83306cac17c20344e113d
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-05-20 16:07:44 +0200

    jak stworzyć funkcje

--- Teoria/14-Funkcje,tworzenie, wywoływanie, parametry z komendami/main.js
+++ Teoria/14-Funkcje,tworzenie, wywoływanie, parametry z komendami/main.js
@@ -12,3 +12,19 @@
 
 // -- można ich używać tam gdzie innych obiektów (są first-class objects, czyli są pełnoprawnymi obiektami) np. przypisać do zmiennych, jako argumentu w innej funkcji czy jako wartość zwracana z funkcji.
 
+// JAK STWORZYĆ FUNKCJE
+
+
+// 1. WYRAŻENIE FUNKCYJNE (funkcja bez nazwy - do funkcji odwołujemy się za pomocą zmiennej). Po angielsku Function Expression.
+
+// Szkielet funkcji 
+// const nazwaFunkcjiCoRobi = function() {} ;
+
+const showMessage = function() {
+    console.log("wywołałem funkcję showMessage")
+   }
+   
+   // showMessage()
+   
+   // taka funkcja bez nazwy określana jest funkcją anonimową.
+   
