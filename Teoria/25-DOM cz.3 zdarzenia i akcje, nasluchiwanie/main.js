ea1131ece9ccad2919e92606b40886125026cddd
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-08-25 20:43:44 +0200

    Metoda addEventListener

--- Teoria/25-DOM cz.3 zdarzenia i akcje, nasluchiwanie/main.js
+++ Teoria/25-DOM cz.3 zdarzenia i akcje, nasluchiwanie/main.js
@@ -18,11 +18,11 @@ window.addEventListener("dblclick", function() {
 //  console.log("skrol");
 // }
 
-function showScroll() {
- console.log("skrol");
-}
+// function showScroll() {
+//  console.log("skrol");
+// }
 
-window.addEventListener("scroll", showScroll);
+// window.addEventListener("scroll", showScroll);
 
 
 
@@ -36,34 +36,34 @@ window.addEventListener("scroll", showScroll);
 
 
 // PRZYKŁAD
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-const addText = function() {
- this.textContent += " :) "; //dodanie znaków (stringa) do istniejacej zawartości tekstowej
- h1.classList.toggle("red"); //dodanie/zabranie klasy z elementu (dodaje jak nie ma, zabiera jak jest)
-}
+// const addText = function() {
+//  this.textContent += " :) "; //dodanie znaków (stringa) do istniejacej zawartości tekstowej
+//  h1.classList.toggle("red"); //dodanie/zabranie klasy z elementu (dodaje jak nie ma, zabiera jak jest)
+// }
 
-h1.addEventListener('click', addText);
-
-
-// PRZYKŁAD - nastawienie nasłuchiwania, czyli uruchomienie metody addEventListener na każdym elemencie. Pamietajmy że nie można beżposrednio na pobranym obiekcie, tylko na każdym jego elemencie osobno
-
-const addClassRed = function() {
- console.log("klik li");
-}
-
-const items = document.querySelectorAll("li");
-// zapis items.addEventListener() nie zadziała, bo nie możemy ustawić nasłuchiwanie na tablicy tylko na pobranych elementach. Jak to zrobić, widać ponizej (można też użyć pętli)
-
-//Metoda forEach sprawia, ze podana jako argument funkcja, jest wykonywana na każdym elemencie tablicy (na każdym li) Przy czym przekazujemy do środka ten element nadając mu nazwę (w tym wypadku item) Przy wywołaniu funkcji na kzdym elemencie w tym item będzie znajdowało się kolejne il (czyli kolejna pozycja z tablicy)
-items.forEach((item) => item.addEventListener("click", addClassRed))
+// h1.addEventListener('click', addText);
 
 
-// TWORZENIE I DODAWANIE ELEMENTÓW DO STRONY
+// // PRZYKŁAD - nastawienie nasłuchiwania, czyli uruchomienie metody addEventListener na każdym elemencie. Pamietajmy że nie można beżposrednio na pobranym obiekcie, tylko na każdym jego elemencie osobno
 
-const divElement = document.createElement('div');
-divElement.textContent = "Ostatni DIV";
-divElement.style.backgroundColor = "red";
-document.body.appendChild(divElement);
+// const addClassRed = function() {
+//  console.log("klik li");
+// }
+
+// const items = document.querySelectorAll("li");
+// // zapis items.addEventListener() nie zadziała, bo nie możemy ustawić nasłuchiwanie na tablicy tylko na pobranych elementach. Jak to zrobić, widać ponizej (można też użyć pętli)
+
+// //Metoda forEach sprawia, ze podana jako argument funkcja, jest wykonywana na każdym elemencie tablicy (na każdym li) Przy czym przekazujemy do środka ten element nadając mu nazwę (w tym wypadku item) Przy wywołaniu funkcji na kzdym elemencie w tym item będzie znajdowało się kolejne il (czyli kolejna pozycja z tablicy)
+// items.forEach((item) => item.addEventListener("click", addClassRed))
+
+
+// // TWORZENIE I DODAWANIE ELEMENTÓW DO STRONY
+
+// const divElement = document.createElement('div');
+// divElement.textContent = "Ostatni DIV";
+// divElement.style.backgroundColor = "red";
+// document.body.appendChild(divElement);
 
 
