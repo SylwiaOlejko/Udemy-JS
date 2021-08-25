d3a86f961f3b58457fd0f2a9f0369f0fc6456522
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-08-25 21:15:20 +0200

    forEach

--- Teoria/25-DOM cz.3 zdarzenia i akcje, nasluchiwanie/main.js
+++ Teoria/25-DOM cz.3 zdarzenia i akcje, nasluchiwanie/main.js
@@ -45,27 +45,31 @@ window.addEventListener("scroll", showScroll);
 
 
 // PRZYKŁAD
-// const h1 = document.querySelector('h1');
+const h1 = document.querySelector('h1');
 
-// const addText = function() {
-//  this.textContent += " :) "; //dodanie znaków (stringa) do istniejacej zawartości tekstowej
-//  h1.classList.toggle("red"); //dodanie/zabranie klasy z elementu (dodaje jak nie ma, zabiera jak jest)
-// }
+const addText = function() {
+ this.textContent += " :) "; //dodanie znaków (stringa) do istniejacej zawartości tekstowej
+ h1.classList.toggle("red"); //dodanie/zabranie klasy z elementu (dodaje jak nie ma, zabiera jak jest)
+}
 
-// h1.addEventListener('click', addText);
+h1.addEventListener('click', addText);
 
 
 // // PRZYKŁAD - nastawienie nasłuchiwania, czyli uruchomienie metody addEventListener na każdym elemencie. Pamietajmy że nie można beżposrednio na pobranym obiekcie, tylko na każdym jego elemencie osobno
 
-// const addClassRed = function() {
-//  console.log("klik li");
-// }
+const addClassRed = function() {
+ console.log("klik li");
+}
 
-// const items = document.querySelectorAll("li");
+const items = document.querySelectorAll("li");
 // // zapis items.addEventListener() nie zadziała, bo nie możemy ustawić nasłuchiwanie na tablicy tylko na pobranych elementach. Jak to zrobić, widać ponizej (można też użyć pętli)
 
-// //Metoda forEach sprawia, ze podana jako argument funkcja, jest wykonywana na każdym elemencie tablicy (na każdym li) Przy czym przekazujemy do środka ten element nadając mu nazwę (w tym wypadku item) Przy wywołaniu funkcji na kzdym elemencie w tym item będzie znajdowało się kolejne il (czyli kolejna pozycja z tablicy)
-// items.forEach((item) => item.addEventListener("click", addClassRed))
+// //Metoda forEach sprawia, ze podana jako argument funkcja, jest wykonywana 
+// na każdym elemencie tablicy (na każdym li) Przy czym przekazujemy do środka 
+// ten element nadając mu nazwę (w tym wypadku item) Przy wywołaniu funkcji na 
+// kzdym elemencie w tym item będzie znajdowało się kolejne il (czyli kolejna
+//  pozycja z tablicy)
+items.forEach((item) => item.addEventListener("click", addClassRed))
 
 
 // // TWORZENIE I DODAWANIE ELEMENTÓW DO STRONY
